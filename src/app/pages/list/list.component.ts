74667601872a02128be145eb4f5d7dd99f7c3820
HectorIdme <felipeidme04@gmail.com>
2023-04-21 16:58:01 -0500

    Add folders and list cards using API

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit{
+
+  constructor(private cardService: CardService){}
+
+  ngOnInit(): void {
+    this.cardService.getCards().subscribe((res) => {
+      console.log(res);
+    });
+  }
+}
